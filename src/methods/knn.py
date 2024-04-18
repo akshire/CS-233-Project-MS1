6eb08f1244c5661896679f8f9983fbf4a6aab5ff
CapraSnitzel <116728837+CapraSnitzel@users.noreply.github.com>
2024-04-18 09:50:52 +0200

    knn maybe done

--- src/methods/knn.py
+++ src/methods/knn.py
@@ -18,15 +18,15 @@ class KNN(object):
             vector and all training_examples.
 
             Inputs:
-                example: shape (D,)
-                training_examples: shape (NxD) 
+                example: shape (,D)
+                training_examples: shape (N, D) 
             Outputs:
                 euclidean distances: shape (N,)
         """
         
         return np.sqrt(((training_examples - example) ** 2).sum(axis=1))
 
-    def label_find(self, test_point, index):
+    def label_find(self, test_point):
         """
             Return the predicted label by calculating all the distances between the point and all the 
             training_data and then returning the most frequent one from k nearest neighbors
@@ -38,7 +38,7 @@ class KNN(object):
                 predicted label: label of the shape
         """
 
-        euclid_distances = self.euclidean_dist(test_point, np.delete(self.train_data, (index), axis = 0))
+        euclid_distances = self.euclidean_dist(test_point, self.train_data)
         kNearest = np.argpartition(euclid_distances, self.k)[:self.k]
         labels = np.zeros(self.k)
         index_kNearest = 0
@@ -71,12 +71,13 @@ class KNN(object):
         ##
         self.train_data = training_data
         self.train_labels = training_labels
+        """
         nbOfData = training_data.shape[0]
         pred_labels = np.zeros(nbOfData)
         for i in range(nbOfData):
             pred_labels[i] = self.label_find(training_data[i, :], i)
-        
-        return pred_labels
+        """
+        return self.predict(training_data)
 
     def predict(self, test_data):
         """
@@ -95,6 +96,6 @@ class KNN(object):
         nbOfData = test_data.shape[0]
         test_labels = np.zeros(nbOfData)
         for i in range(nbOfData):
-            test_labels[i] = self.label_find(test_data[i, :], i)
+            test_labels[i] = self.label_find(test_data[i, :])
             
         return test_labels
